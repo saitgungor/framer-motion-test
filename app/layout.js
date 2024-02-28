bc6727434cfbf3fe2e7157e47df16ea11a7b1bd8
Osman Sait Güngör <osmansaitgungor@gmail.com>
2024-02-28 17:29:42 +0300

    Trying animations

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,9 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="w-screen h-screen bg-gradient-to-br from-blue-900 to blue-500">{children}</body>
+      <body className="w-screen h-screen bg-gradient-to-br from-blue-900 to blue-500 flex justify-center items-center">
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,3 +1,47 @@
+"use client";
+import { AnimatePresence, motion, useAnimate, useAnimationControls, useInView } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const container = {
+  hidden: {
+    transition: {
+      staggerChildren: 0.3, // Adjust this value for the desired delay between children
+      staggerDirection: -1, // Adjust this value for the desired delay between children
+    },
+  },
+  visible: {
+    transition: {
+      staggerChildren: 0.3, // Adjust this value for the desired delay between children
+    },
+  },
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
 export default function Home() {
-  return <div>asd</div>;
+  const controls = useAnimationControls();
+  console.log(controls);
+  return (
+    <AnimatePresence>
+      <motion.ul
+        className="w-36 h-36 grid grid-cols-2 gap-4 p-4 bg-gray-300 bg-opacity-30 overflow-hidden m-0 list-none rounded-lg"
+        variants={container}
+        initial="visible"
+        animate={controls}
+        exit={{ scale: 0, x: [0, 100, 0] }}
+        onMouseEnter={() => controls.start("hidden")}
+        onMouseLeave={() => controls.start("visible")}
+      >
+        {[0, 1, 2, 3].map((index) => (
+          <motion.li key={index} className="bg-white rounded-full" variants={item} />
+        ))}
+      </motion.ul>
+    </AnimatePresence>
+  );
 }
