8a935d1d09a6a95a929c5c853dad8c8d44dde40d
Osman Sait Güngör <osmansaitgungor@gmail.com>
2024-02-29 15:57:48 +0300

    Updating animation

--- app/example1/index.jsx
+++ app/example1/index.jsx
@@ -7,6 +7,25 @@ import { useDimensions } from "../use-dimensions";
 
 const sidebar = {
   open: (height = 1000) => ({
+    transition: {
+      ease: "linear",
+      stiffness: 20,
+      restDelta: 2,
+      duration: 0.5,
+    },
+    width: 356,
+  }),
+  closed: {
+    transition: {
+      type: "",
+      stiffness: 400,
+      // damping: 40,
+    },
+  },
+};
+
+const sidebar2 = {
+  open: (height = 1000) => ({
     clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
     transition: {
       type: "spring",
@@ -29,9 +48,16 @@ const Example1 = () => {
   const [isOpen, toggleOpen] = useCycle(false, true);
   const containerRef = useRef(null);
   const { height } = useDimensions(containerRef);
+  console.log(height);
   return (
-    <motion.nav initial={false} animate={isOpen ? "open" : "closed"} custom={height} ref={containerRef}>
-      <motion.div className="background" variants={sidebar} />
+    <motion.nav
+      initial={false}
+      animate={isOpen ? "open" : "closed"}
+      custom={height}
+      ref={containerRef}
+      className="left-0 top-0 absolute bottom-0"
+    >
+      <motion.div className="bg-slate-100 absolute left-0 top-0 bottom-0" variants={sidebar} />
       <Navigation />
       <MenuToggle toggle={() => toggleOpen()} />
     </motion.nav>
